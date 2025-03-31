de73cfc635650425b3929c3196e31bc84d9cf292
Leo <68201109+camle2145@users.noreply.github.com>
2025-03-31 18:20:06 +1300

    Create repository

---
+++ src/main/java/com/nfl/fantasy_stats/FantasyStatsApplication.java
@@ -0,0 +1,13 @@
+package com.nfl.fantasy_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nfl/fantasy_stats/FantasyStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfl.fantasy_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
